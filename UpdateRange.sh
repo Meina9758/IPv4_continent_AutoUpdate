82846ccd7125c269d55fe0e0fbce4f04c2f2fd84
Meina9758 <bilibilimyhx@gmail.com>
2023-02-07 04:12:59 +0900

    update

--- UpdateRange.sh
+++ UpdateRange.sh
@@ -1,2 +1,21 @@
 IP_Trans=python ./serialize/main.py;
-git install -y cidrize
+
+
+if [ ! -d "./Asia" ]; then
+  mkdir /Asia
+fi
+if [ ! -d "./Europe" ]; then
+  mkdir /Europe
+fi
+if [ ! -d "./NorthAmerica" ]; then
+  mkdir /NorthAmerica
+fi
+if [ ! -d "./SouthAmerica" ]; then
+  mkdir /SouthAmerica
+fi
+if [ ! -d "./Africa" ]; then
+  mkdir /Africa
+fi
+if [ ! -d "./Oceania" ]; then
+  mkdir /Oceania
+fi
